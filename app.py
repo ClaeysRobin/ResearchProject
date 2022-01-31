0754d56ab5933989c51432379745349c4595a346
Robin <robin.claeys2@student.howest.be>
2022-01-31 20:54:17 +0100

    update

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import *
 import VQGAN
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def  HomePage():
-    # shows the index.html file in the templates folder
     return render_template("Index.html")
 
 @app.route("/Overview")
 def  OverviewPage():
-    # shows the index.html file in the templates folder
     return render_template("ImgOverview.html")
 
 @app.route("/runscript/<InputText>", methods=['GET'])
@@ -20,7 +16,6 @@ def ScriptPage(InputText):
     VQGAN.run_model(InputText)
     # print(InputText)
     return redirect(url_for("OverviewPage"))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
